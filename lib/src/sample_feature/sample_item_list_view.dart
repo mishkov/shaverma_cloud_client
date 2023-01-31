269c9d486809743e22290ec5fa86e133892408ed
Nikita Mishkov <mishkov.dd@gmail.com>
2023-01-31 17:10:36 +0300

    Implement fetching available movies from server

--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -1,11 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 import '../settings/settings_view.dart';
 import 'sample_item.dart';
-import 'sample_item_details_view.dart';
 
-import 'package:video_player_macos/video_player_macos.dart';
+import 'package:http/http.dart' as http;
 
 class SampleItemListView extends StatefulWidget {
   const SampleItemListView({
@@ -22,23 +23,46 @@ class SampleItemListView extends StatefulWidget {
 }
 
 class _SampleItemListViewState extends State<SampleItemListView> {
+  Uri baseUri = Uri.parse('http://localhost:8080/');
   late VideoPlayerController _controller;
+  Future<List<String>>? _availableMoviesFetchFuture;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(
-        'http://localhost:8080/')
+    _controller = VideoPlayerController.network('http://localhost:8080/')
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
+
+    _availableMoviesFetchFuture = _getAvailableMovies();
   }
 
   @override
   void dispose() {
     super.dispose();
     _controller.dispose();
+  }
+
+  Future<List<String>> _getAvailableMovies() async {
+    final response = await http.get(
+      baseUri.replace(path: '/get-available-movies'),
+    );
+    if (response.statusCode != 200) {
+      throw Exception();
+    }
+
+    final Map<String, dynamic> mapResponse = jsonDecode(response.body);
+    if (!mapResponse.containsKey('paths')) {
+      throw Exception();
+    }
+
+    final pathsToMovies = (mapResponse['paths'] as List).map((path) {
+      return path.toString();
+    }).toList();
+
+    return pathsToMovies;
   }
 
   @override
@@ -55,12 +79,59 @@ class _SampleItemListViewState extends State<SampleItemListView> {
           ),
         ],
       ),
-      body: _controller.value.isInitialized
-          ? AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
-            )
-          : Container(),
+      body: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _availableMoviesFetchFuture = _getAvailableMovies();
+                    });
+                  },
+                  child: const Text('Relod Movies List'),
+                ),
+                Expanded(
+                  child: FutureBuilder(
+                    future: _availableMoviesFetchFuture,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        final movies = snapshot.data;
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: movies?.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(movies?[index] ?? 'Error'),
+                              ),
+                            );
+                          },
+                        );
+                      } else {
+                        return const Center(
+                          child: Text('Error!!!'),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: _controller.value.isInitialized
+                ? AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  )
+                : Container(),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
